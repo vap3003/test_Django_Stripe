53a68217e33482afb64624d209615217bace32a3
Владислав Павленко <vap3003@vk.com>
2023-02-10 22:50:33 +0400

    try to fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web gunicorn djangostripe.wsgi:application --log-file -
-release: python djangostripe/manage.py makemigrations --noinput && python djangostripe/manage.py migrate --noinput
+release: python djangostripe/manage.py migrate
--- djangostripe/djangostripe/settings.py
+++ djangostripe/djangostripe/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tap-django-stripe.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['tap-django-stripe.herokuapp.com']
 
 
 # Application definition
