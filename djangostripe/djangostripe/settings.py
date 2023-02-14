991b508eca2ef5d95dcb2d647873d80c8273855e
Владислав Павленко <vap3003@vk.com>
2023-02-14 14:38:03 +0400

    heroku

--- djangostripe/djangostripe/settings.py
+++ djangostripe/djangostripe/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic',
     'payments.apps.PaymentsConfig',
 ]
 
@@ -153,7 +152,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Test Runner Config
 class HerokuDiscoverRunner(DiscoverRunner):
