fa055c853956c52e72e69b68bd56e13d408619f6
Владислав Павленко <vap3003@vk.com>
2023-02-14 16:00:53 +0400

    stripe

--- djangostripe/payments/views.py
+++ djangostripe/payments/views.py
@@ -7,6 +7,7 @@ from django.views.generic.base import TemplateView
 import stripe
 from .models import Item
 from django.views import View
+from django.urls import reverse
 
 
 def item(request, id):
@@ -23,8 +24,8 @@ def item(request, id):
 def buy(request, id):
     if request.method == 'GET':
         item = get_object_or_404(Item, id=id)
-        domain = "https://tap-django-stripe.herokuapp.com"
-        stripe.api_key = settings.STRIPE_SECRET_KEY
+        domain = 'https://tap-django-stripe.herokuapp.com'
+        stripe.api_key = settings.STRIPE_PUBLISHABLE_KEY
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
@@ -39,10 +40,36 @@ def buy(request, id):
             success_url = domain + '/success/',
             cancel_url = domain + '/cancel/',
         )
-        stripe.redirectToCheckout({ 'sessionId': checkout_session.id })
+        return stripe.redirectToCheckout( 'sessionId': checkout_session.id)
         # return JsonResponse({'sessionId': checkout_session['id']})
 
 
         #     return JsonResponse({'sessionId': checkout_session['id']})
         # except Exception as e:
         #     return JsonResponse({'error': str(e)})
+
+@csrf_exempt
+def create_checkout_session(request, id):
+    item = get_object_or_404(Item, pk=id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                    'name': item.name,
+                    },
+                    'unit_amount': int(item.price * 100),
+                },
+                'quantity': 1,
+            }
+        ],
+        mode='payment',
+        success_url=request.build_absolute_uri(
+            reverse('success')
+        ) + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(reverse('failed')),
+    )
+    return JsonResponse({'sessionId': checkout_session.id})
